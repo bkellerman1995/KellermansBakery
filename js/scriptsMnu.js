697c46b0b34340e14554cf102437986e69e7d18f
bkellerman1995 <bkellerman@microsoft.com>
2024-11-19 18:03:35 -0600

    Se agregó la lógica para incluir, quitar productos en el menú. También se agregó la lógica para sumar el subtotal y el total de los productos en el carrito antes de completar la compra. También se agregó un botón en el carrito para eliminar el producto del carrito.

--- js/scriptsMnu.js
+++ js/scriptsMnu.js
@@ -1,6 +1,20 @@
 //JAVA SCRIPT PARA CARGAR PRODUCTOS POR CATEGORIA EN PAGINA MNU
 
+
 // Cargar el archivo JSON
+
+// Eventos de carga inicial
+$(document).ready(() => {
+
+    animateCartIcon();
+
+    // $("#view-products").on("click", function () {
+    //     displayProductTable(); // Mostrar la tabla de productos al hacer clic
+    // });
+
+    // updateCartIndicator();
+});
+
 fetch('../json/productos.json')
     .then(response => {
         if (!response.ok) throw new Error('No se pudo cargar el archivo JSON');
@@ -77,15 +91,15 @@ fetch('../json/productos.json')
                                                                     style= "position: relative; left: 50%;
                                                                     transform: translate(-50%, -50%);
                                                                         border: 1px solid #ced4da; background-color: #8B4513;color: white;width: 10rem;border-radius: .25rem;">
-                              <button data-mdb-button-init onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
-                                class="minus"></button>
+                              <button data-mdb-button-init onclick="this.parentNode.querySelector('input[type=number]').stepDown(); updateButtonState()"
+                                class="minus" id="botonMenosMenu"></button>
                               <input class="quantity fw-bold bg-body-tertiary text-body" min="0" name="quantity" value="1"
                                 type="number">
-                              <button data-mdb-button-init onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
-                                class="plus"></button>
+                              <button data-mdb-button-init onclick="this.parentNode.querySelector('input[type=number]').stepUp(); updateButtonState()"
+                                class="plus" id="botonMasMenu"></button>
                             </div>
 
-                                        <button class="btn btn-success" onclick="addToCart('${producto.nombre}')">Agregar al carrito</button>
+                                        <button class="btn btn-success" onclick="addToCart('${producto.id}')">Agregar al carrito</button>
                 
                                         <button class="btn btn-info" onclick="showDetails('${producto.id}')"
                                         style = "background-color: #8B4513; color: white; border-style:none" >Mostrar detalles</button>
@@ -99,27 +113,66 @@ fetch('../json/productos.json')
         };
 
         //Función para cerrar el modal 
-
         window.cerrarModal = function () {
         }
 
-        // Función para agregar al carrito
-        window.addToCart = function (productName) {
-            Swal.fire({
-                icon: 'success',
-                title: 'Producto agregado',
-                text: `${productName} ha sido agregado al carrito.`,
-                showConfirmButton: false,
-                timer: 1500
-            });
-        };
+        // Función para agregar productos al carrito
+        window.addToCart = function (id) {
 
-        // Función para mostrar detalles en el modal
-        window.showDetails = function (id) {
-            const producto = data.find(item => item.id === id);
-            if (producto) {
-                const modalContent = document.getElementById("modalContent");
-                modalContent.innerHTML = `
+            const producto = data.find(item => item.id === id)
+            //Parsear la cantidad de producto desde el campo input
+            const quantity = parseInt(document.querySelector('.quantity').value);
+            if (quantity <= 0) {
+                alert("La cantidad debe ser mayor a 0.");
+                return;
+            }
+
+            //Obtener el carrito del almacenamiento local del navegador o inicializarlo
+            let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+            // Buscar si el producto ya existe en el carrito
+            const existeProducto = cart.findIndex(item => item.id === id);
+
+            if (existeProducto !== -1) {
+                // Si el producto ya existe, actualiza su cantidad y precio
+                cart[existeProducto].cantidad += quantity;
+                cart[existeProducto].subtotal = producto.precio * cart[existeProducto].cantidad;
+
+            } else {
+
+                //Si el producto no existe, prepara los datos del producto para agregarlo
+                const cartItem = {
+                    id: producto.id,
+                    nombre: producto.nombre,
+                    cantidad: quantity,
+                    precio: producto.precio,
+                    subtotal: producto.precio * quantity,
+                    categoria: producto.categoria,
+                    descripcion: producto.descripcion,
+                    imagen: producto.imagen
+                };
+
+                //Agregar el item al carrito
+                cart.push(cartItem);
+            }
+
+                //Salvar el carrito actualizado al almacenamiento local
+                localStorage.setItem('cart', JSON.stringify(cart));
+
+                //Desplegar la alarta
+                Swal.fire('Producto agregado', `${producto.nombre} ha sido agregado al carrito`, 'success');
+
+                //Chequear si el carrito tiene items para animarlo
+
+                animateCartIcon();
+            }
+
+            // Función para mostrar detalles en el modal
+            window.showDetails = function (id) {
+                const producto = data.find(item => item.id === id);
+                if (producto) {
+                    const modalContent = document.getElementById("modalContent");
+                    modalContent.innerHTML = `
                             <p><strong>Código:</strong> ${producto.id}</p>
                             <p><strong>Nombre:</strong> ${producto.nombre}</p>
                             <img src = ${producto.imagen} alt="imagen de comida" 
@@ -130,19 +183,66 @@ fetch('../json/productos.json')
                             <p><strong>Descripción:</strong> ${producto.descripcion}</p>
                             <p><strong>Categoría:</strong> ${producto.categoria}</p>
                         `;
-                $('#productModal').modal('show');  // Mostrar el modal
+                    $('#productModal').modal('show');  // Mostrar el modal
+                }
             }
-        };
 
-        // Mostrar productos de "Categoria 1" al cargar la página, si existe
-        if (categorias.includes('Categoria 1')) {
-            showProducts('Categoria 1');
-        }
+            // Mostrar productos de "Categoria 1" al cargar la página, si existe
+            if (categorias.includes('Categoria 1')) {
+                showProducts('Categoria 1');
+            };
 
 
-    })
+        })
     .catch(error => {
         console.error('Error al cargar productos:', error);
         document.getElementById("categories").innerHTML = '<p class="col-12 text-center">No hay categorías disponibles</p>';
         document.getElementById("products").innerHTML = '<p class="col-12 text-center">No hay productos disponibles</p>';
     });
+
+//Función para chequear y actualizar el estado del botón "Agregar al carrito"
+function updateButtonState() {
+    //Seleccionar el input de número (clase quantity) y el botón de agregar al carrito (clase btn-success)
+    const quantityInput = document.querySelector('.quantity');
+    const addToCartButton = document.querySelector('.btn-success');
+
+    if (quantityInput.value <= 0) {
+        addToCartButton.disabled = true; // Deshabilita el botón
+    } else {
+        addToCartButton.disabled = false; // Habilita el botón
+    }
+
+    //Agregar los listeners al input y a los botones "-" y "+" (.minus y .plus)
+    quantityInput.addEventListener('input', updateButtonState); // Lanzar el evento cuando hay un cambio en el input
+    document.querySelector('.minus').addEventListener('click', updateButtonState); // Lanzar el evento cuando se hace clic en "-"
+    document.querySelector('.plus').addEventListener('click', updateButtonState); // Lanzar el evento cuando se hace clic en "+"
+
+};
+
+// Función para chequear si existen artículos en el carrito
+function hasCartItems() {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    return cart.length > 0;
+}
+
+// Función para animar el carrito de compras
+function animateCartIcon() {
+    const cartIcon = document.querySelector('.fa.fa-shopping-cart');
+
+    if (hasCartItems() > 0) {
+        cartIcon.classList.add('animate');
+    }
+    else {
+        cartIcon.classList.remove('animate');
+    }
+}
+
+//Llamado de la función siempre que el carro se actualice
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('.btn.btn-success');
+    if (button) {
+        button.addEventListener('click', () => {
+            animateCartIcon();
+        });
+    }
+});
